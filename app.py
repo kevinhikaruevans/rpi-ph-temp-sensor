e882d078ff50a1d7d4f44cae24afc78c372a48a5
Kevin Evans <evans.kevinh@gmail.com>
2022-02-02 13:26:25 -0800

    set time to fixed val

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ parser = argparse.ArgumentParser(description='Read sensor data and send to API')
 parser.add_argument('--dry-run', action='store_true', help='Do not send data to API')
 args = parser.parse_args()
 
-def perform_api_request(sensor, value):
+def perform_api_request(sensor, value, timestamp=None):
     requests.post(f"https://{API_HOST}/api/entries/push", timeout=API_TIMEOUT, json={
         "token": API_TOKEN,
         "sensor": sensor,
@@ -35,7 +35,11 @@ if __name__ == "__main__":
         if args.dry_run:
             perform_dry_run()
         else:
-            perform_api_request("Temperature", read_temp())
-            perform_api_request("pH", read_ph())
+            timestamp = int(time.time())
+            temp = read_temp()
+            pH = read_ph()
+
+            perform_api_request("Temperature", temp, timestamp)
+            perform_api_request("pH", pH, timestamp)
 
         time.sleep(POLL_DELAY)
